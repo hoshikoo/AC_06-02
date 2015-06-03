1a3a49a896f88b3f80dd3069ce600c74011ed880
hoshikoo <hoshikooki@gmail.com>
2015-06-02 23:46:28 -0400

    initial commit

---
+++ app/src/main/java/hoshikoo/c4q/nyc/ac_06_02/MainActivity.java
@@ -0,0 +1,60 @@
+package hoshikoo.c4q.nyc.ac_06_02;
+
+import android.content.res.Resources;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Resources res = getResources();
+        String[] strList = res.getStringArray(R.array.lists);
+
+
+
+        ArrayAdapter<String> itemsAdapter =
+                new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, strList);
+
+//        ListView listView = (ListView) findViewById(R.id.listOfItems);
+//        listView.setAdapter(itemsAdapter);
+
+
+        GridView grView = (GridView)findViewById(R.id.grid_view);
+        grView.setAdapter(itemsAdapter);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
